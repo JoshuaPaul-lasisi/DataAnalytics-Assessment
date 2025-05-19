a2c35e72c4cbffa9c03c5621dcd4efbe59fba336
Tobi Joshua <jpaullasisi@gmail.com>
2025-05-19 01:02:59 +0100

    Assessment 2

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d527f4a6a87a623d0256b09a667f7632525ff0073dc55fb0e0d5cbbcca054dde
-size 1065
+oid sha256:ee2dfa8add8db6f0cd2cfad8f21bc5cf4cf3acc3131fcbb61b35ba73d24b0fd8
+size 1149
